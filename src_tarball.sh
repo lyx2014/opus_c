77feef0e1dc485e354344d274fb0ac3174a5b15f
Ken Rice <krice@freeswitch.org>
2015-05-11 16:06:16 -0500

    add src_tarball script

---
+++ src_tarball.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ORIGDIR=`pwd`
+TMPDIR=opus.$$
+
+mkdir -p ../${TMPDIR}
+
+./autogen.sh
+
+cd ..
+cp -a opus ${TMPDIR}/opus-1.1
+cd ${TMPDIR}
+rm -rf opus-1.1/.git*
+tar zcvf opus-1.1.tar.gz opus-1.3.0
+mv opus-1.1.tar.gz ${ORIGDIR}/.
+cd ${ORIGDIR}
+rm -rf ../${TMPDIR}
