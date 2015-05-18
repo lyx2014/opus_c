d2222e50ed629da000b6c3c32c4d9f1c9c9720cd
Ken Rice <krice@freeswitch.org>
2015-05-18 14:51:12 -0500

    small tweak to make src_tarball.sh actually generate something usable

--- src_tarball.sh
+++ src_tarball.sh
@@ -10,7 +10,7 @@ cd ..
 cp -a opus ${TMPDIR}/opus-1.1
 cd ${TMPDIR}
 rm -rf opus-1.1/.git*
-tar zcvf opus-1.1.tar.gz opus-1.3.0
+tar zcvf opus-1.1.tar.gz opus-1.1
 mv opus-1.1.tar.gz ${ORIGDIR}/.
 cd ${ORIGDIR}
 rm -rf ../${TMPDIR}
